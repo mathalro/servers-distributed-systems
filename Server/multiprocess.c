5ce1efe6e94ee1be14b7830e82e820d614714f12
MatheusAlmeidaRosa <matheus.ar11@hotmail.com>
2018-09-03 18:07:29 -0300

    thread & multiprocess server

---
+++ Server/multiprocess.c
@@ -0,0 +1,59 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h> 
+
+void error(char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main (int argc, char **argv) {
+
+	int listenfd, connfd, nbuf, i, pid;
+	int clilen;
+	short port, maxcon = 5;
+
+	struct sockaddr_in cliaddr, servaddr;
+
+	char buf[256];
+	
+	if (argc < 2) {
+		printf("Please, specify the port number.");
+		exit(1);
+	}
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (listenfd < 0) error("Error to open socket.");
+	
+	bzero((char *) &servaddr, sizeof(servaddr));
+	port = atoi(argv[1]);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) error("Error to bind socket to address.");
+
+	listen(listenfd, maxcon);
+	for (i = 1;;i++) {
+		printf("Request %d:\n", i);
+		clilen = sizeof(cliaddr);
+		connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
+
+		if (pid < 0) error("Error to fork process");
+		else if ((pid = fork()) == 0) {
+			while ( (nbuf = recv(connfd, buf, sizeof(buf), 0)) > 0) {
+				printf("Received from request %d: %s\n", i, buf);
+				send(connfd, buf, nbuf, 0);
+				sleep(5);
+			}
+			if (nbuf < 0) error("Read error");
+			exit(0);
+		}
+		close(connfd);
+	}
+}
---
+++ Server/multithread.c
@@ -0,0 +1,72 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h> 
+#include <pthread.h>
+
+#define NUM_THREADS 1
+#define BUF_SIZSE 256
+
+void error(char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+void *job(void *con) {
+	long nbuf;
+	char buf[BUF_SIZSE];
+	long connfd = (long) con;
+
+	while ((nbuf = recv(connfd, buf, sizeof(buf), 0)) > 0) {
+		printf("Received message from client %ld: %s\n", connfd, buf);
+		send(connfd, buf, nbuf, 0);
+	}
+
+	if (nbuf < 0) error("Read error");
+
+	close(connfd);
+
+	pthread_exit(NULL);
+} 
+
+int main (int argc, char **argv) {
+
+	pthread_t thread;
+	long connfd, i, tid, listenfd;
+	int clilen;
+	short port, maxcon = 5;
+
+	struct sockaddr_in servaddr, cliaddr;
+	char buf[BUF_SIZSE];
+	
+	if (argc < 2) {
+		printf("Please, specify the port number.");
+		exit(1);
+	}
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (listenfd < 0) error("Error to open socket.");
+	
+	bzero((char *) &servaddr, sizeof(servaddr));
+	port = atoi(argv[1]);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) error("Error to bind socket to address.");
+
+	listen(listenfd, maxcon);
+	for (i = 0;;i++) {
+		printf("Waiting for request...\n");
+		clilen = sizeof(cliaddr);
+		connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
+		printf("Received request from %ld:\n", connfd);
+		if ((tid = pthread_create(&thread, NULL, job, (void *)connfd ))) {
+			error("Error to create Thread");
+		}
+	}
+}
